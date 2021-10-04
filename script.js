ff7ebc1a06253bf2578f3f9f56498d081c1dce2a
Andrew Cho <andrewgunncho@gmail.com>
2021-10-04 15:42:22 -0700

    k6 tests and new relic test up

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-  vus: 1, // stimulate virtual users
+  vus: 100, // stimulate virtual users
   duration: '30s', // test duration
 };
 
