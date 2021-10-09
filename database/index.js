029778cb5ee640b631641ea5258b1edf0386f72d
Andrew Cho <andrewgunncho@gmail.com>
2021-10-08 19:20:55 -0700

    Finished

--- database/index.js
+++ database/index.js
@@ -14,138 +14,14 @@ pool.on('error', (err, client) => {
   console.error('Error:', err);
 });
 
-// const getRating = (id) => {
-  // obj
-  // var obj = {};
-
-  // loop through ratings in reviews where product_id
-    // set rating with value as key and increment
-      // if it exists, increment
-
-  // return object // <- attach this to ratings
-// }
-
-// const getRecommend = (id) => {
-  // obj
-  // var obj = {};
-
-  // loop through recommend in reviews where product_id
-    // set key as boolean and increment
-      // if it exists, increment
-
-  // return object // <- attach this to recommended
-// }
-
-// VALUES ${photosQuery('id', 'reviewsIns', 'photos')}
-// VALUES ${characteristicsQuery('id', 'reviewsIns')}
-// separate the entries for photos and characteristics
-
-// get: (params, callback) => {
-//   pool.query('SELECT id, rating, summary, recommend, response, body, date, reviewer_name, helpfulness FROM reviews WHERE product_id = $1 AND reported IS NOT TRUE LIMIT 5', params, (err, data) => {
-//     if (err) {
-//       callback (err, null)
-//     } else {
-//       callback (null, data.rows)
-//     }
-//   })
-// },
-
-// const characteristics = (id) => {
-//   `SELECT * FROM characteristics WHERE product_id = ${id}`
-// }
-
-// let obj = {
-//   `product_id: ${data.id},
-//   ratings: ${},
-//   recommended: ${},
-//   characteristics: ${characteristics()}`
-// }
-
 module.exports = {
   get: (text, callback) => {
     pool.query(text, null, (err, data) => {
-      // console.log(err)
       if (err) {
         callback (err, null)
       } else {
         callback (null, data.rows)
       }
     })
-  },
-
-  getMeta: (text, callback) => {
-    pool.query(text, null, (err, data) => {
-      console.log(err)
-      if (err) {
-        callback (err, null)
-      } else {
-        obj[data]
-        callback (null, obj)
-      }
-    })
-  },
-
-  // WITH reviews AS (
-  //   INSERT INTO reviews (product_id, rating, date, summary, body, recommend, reviewer_name, reviewer_email)
-  //   VALUES (4, 4, 123423152315, 'lol', 'test', true, 'a', 'a@a.com')
-  //   RETURNING id
-  //   )
-  // , photos AS (
-  //   INSERT INTO photos (url, review_id)
-  //   VALUES (['https//:gottem.com', 'https//:lmao.com'], id) <- need to separate
-  // , characteristics_review AS (
-  //   INSERT INTO ()
-  //   VALUES ()
-  // )
-
-
-
-  post: (params, callback) => {
-    pool.query('INSERT INTO reviews (product_id, rating, date, summary, body, recommend, reviewer_name, reviewer_email, photos, characteristics) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)', params, (err, data) => {
-      if (err) {
-        callback (err, null);
-      } else {
-        callback (null, data);
-      }
-    })
-  },
-
-  put: (params, callback) => {
-    pool.query('UPDATE reviews SET helpful = helpful + 1 WHERE review_id = ?', params, (err, data) => {
-      if (err) {
-        callback (err, null);
-      } else {
-        callback (null, data);
-      }
-    })
-  },
-
-  report: (params, callback) => {
-    pool.query('UPDATE reviews SET reported = true WHERE reported = false', params, (err,data) => {
-      if (err) {
-        callback (err, null);
-      } else {
-        callback (null, data);
-      }
-    })
   }
-
 }
-
-// (async () => {
-//   const client = await pool.connect();
-//   const query = 'SELECT * FROM reviews';
-
-//   const cursor = await client.query(new Cursor(query));
-
-//   cursor.read(1, (err, rows) => {
-//     console.log('We got the first row set');
-//     console.log(rows);
-
-//     cursor.read(1, (err, rows) => {
-//       console.log('This is the next row set');
-//       console.log(rows);
-//     });
-//   });
-// })();
-
--- server/index.js
+++ server/index.js
@@ -9,21 +9,6 @@ const app = express();
 
 app.use(express.json());
 
-// const pagesQuery = {
-//   text: `
-//     SELECT * FROM reviews
-//     WHERE product_id=${id}
-//     ORDER BY id
-//     OFFSET (($1 - 1) * $2) ROWS
-//     FETCH NEXT $2 ROWS ONLY
-//   `,
-//   values: [page, count],
-// };
-
-// CASE WHEN photos is null
-// THEN '[]'
-// ELSE 'id', id, 'url', url END
-
 app.get('/loaderio-cd4244f3aa8828e1a6fef5970d3856b5', (req, res) => {
   res.send('loaderio-cd4244f3aa8828e1a6fef5970d3856b5');
 })
@@ -65,105 +50,6 @@ app.get('/reviews/', (req, res) => {
     }
   })
 })
-
-// first attempt
-
-// `SELECT json_build_object(
-//   'product_id', ${req.query.product_id},
-//   'ratings', 1,
-//   'recommended', 1,
-//   'characteristics',
-//     (SELECT json_agg(json_build_object(
-//       name,
-//       (SELECT json_agg(json_build_object(
-//         'id', id,
-//         'value', value
-//       ))
-//       FROM characteristics_review
-//       WHERE review_id = review.id
-//       AND characteristic_id = characteristics.id)
-//     ))
-//     FROM characteristics
-//     WHERE product_id = ${req.query.product_id})
-//   FROM reviews
-//   WHERE product_id = ${req.query.product_id})`
-
-
-// ---
-
-// `SELECT json_build_object(
-//   'product_id', ${req.query.product_id},
-//   'ratings', 1,
-//   'recommended', 1,
-//   'characteristics',
-//     (SELECT json_agg(row_to_json(characteristics))
-//     FROM (SELECT name,
-//       (SELECT json_build_object(
-//       'id', id,
-//       'value', value
-//       )
-//       FROM characteristics_review
-//       WHERE characteristic_id = characteristics.id
-//       AND review_id = reviews.id)
-//       AS characteristics_review
-//     FROM characteristics
-//     WHERE product_id = ${req.query.product_id})
-//     AS characteristics)
-//   FROM reviews
-//   WHERE product_id = ${req.query.product_id}
-//   AS reviews)`;
-
-
-app.get('/reviews/meta', (req, res) => {
-
-  // var text =
-  //   `SELECT json_build_object(
-  //     'product_id', 1,
-  //     'ratings', 1,
-  //     'recommended', 1,
-  //     'characteristics',
-  //       (SELECT json_agg(row_to_json(characteristics))
-  //       FROM (SELECT name,
-  //         (SELECT json_build_object(
-  //         'id', id,
-  //         'value', value
-  //         ) AS test
-  //         FROM characteristics_review
-  //         WHERE characteristic_id = 1
-  //         AND review_id = 1)
-  //         AS characteristics_review
-  //       FROM characteristics
-  //       WHERE product_id = 1)
-  //       AS characteristics)
-  //     FROM reviews
-  //     WHERE product_id = 1
-  //     AS reviews)`;
-
-
-  db.getMeta(text, (err, data) => {
-    if (err) {
-      res.send(err)
-    } else {
-      res.send(data[0].json_build_object);
-    }
-  })
-})
-
-app.post('/reviews', (req, res) => {
-  console.log(req.body)
-  var params = [req.body.product_id, req.body.rating, new Date(), req.body.summary, req.body.body, req.body.recommend, req.body.name, req.body.email, req.body.photos, req.body.characteristics];
-  console.log(params)
-  // db.getMeta(params, (err, data) => {
-  //   if (err) {
-  //     res.send(err)
-  //   } else {
-  //     res.sendStatus(201);
-  //   }
-  // })
-})
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Port:${PORT} brings me eternal suffering`);
